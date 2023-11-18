a158c4fa046a8e5a7c8a03d782eaa4ccbc471875
shshah135 <shailys867@gmail.com>
2023-11-18 17:23:45 -0500

    updating eslint file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // main.js
-const { FileProcessor } = require('./txtToHtml')
+//const { FileProcessor } = require('./txtToHtml')
 const {
   processFile,
   generateSidebar,
@@ -11,34 +11,30 @@ const { parseCommandLineArgs } = require('./cli')
 function main() {
   try {
     const args = process.argv.slice(2)
-    const { lang, outputDir, inputPath } = parseCommandLineArgs(args);
+    const { lang, outputDir, inputPath } = parseCommandLineArgs(args)
 
     if (!inputPath) {
-      throw new Error('Error: Input file or directory path not provided.');
+      throw new Error('Error: Input file or directory path not provided.')
     }
 
     if (fs.existsSync(inputPath)) {
       if (fs.lstatSync(inputPath).isDirectory()) {
-        throw new Error('Error: Input file or directory not found.');
+        throw new Error('Error: Input file or directory not found.')
       }
 
-        // Generate a sidebar and index page
-        const files = fs.readdirSync(outputDir)
-        generateSidebar(files, outputDir)
-        generateIndexPage(files, outputDir)
-      } else {
-        processFile(inputPath, outputDir, lang)
-      }
-      console.log('Conversion completed successfully.')
-      process.exit(0) // Exit with success code
+      // Generate a sidebar and index page
+      const files = fs.readdirSync(outputDir)
+      generateSidebar(files, outputDir)
+      generateIndexPage(files, outputDir)
     } else {
-      console.error('Error: Input file or directory not found.')
-      process.exit(1) // Exit with an error code
+      processFile(inputPath, outputDir, lang)
     }
+    console.log('Conversion completed successfully.')
+    process.exit(0) // Exit with success code
   } catch (error) {
-    console.error(`Unexpected Error: ${error.message}`);
-    process.exit(1); // Exit with an error code
+    console.error(`Unexpected Error: ${error.message}`)
+    process.exit(1) // Exit with an error code
   }
 }
 
-module.exports = main;
+module.exports = main
